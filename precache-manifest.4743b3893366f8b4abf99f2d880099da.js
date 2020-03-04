060933b9c23324fdbc100e6d318d03d760da7b90
Emmanuel Gomes <aeag@outlook.com>
2020-03-04 15:02:54 -0300

    Updates

---
+++ precache-manifest.4743b3893366f8b4abf99f2d880099da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2362266a43b4032883c3e2bfbb1a4c8e",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "5a51b6b56d3820deed4c",
+    "url": "/rolodex/static/css/main.046ee562.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a51b6b56d3820deed4c",
+    "url": "/rolodex/static/js/main.f358ba4e.chunk.js"
+  },
+  {
+    "revision": "9a7203850b119fcfde7b",
+    "url": "/rolodex/static/js/runtime-main.eb357a42.js"
+  }
+]);
